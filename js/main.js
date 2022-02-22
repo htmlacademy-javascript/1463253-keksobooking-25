a127d64efe761f43c6b6a525d3db94f508c1ec53
Ilya Gruzdev <gruzdevilyaa@gmail.com>
2022-02-22 17:39:49 +0300

    Завершил домашнее задание

--- js/main.js
+++ js/main.js
@@ -1,17 +1,68 @@
-'use strict'
+/**
+ * Получаем случайное целое число из диапазона
+ * @param {number} min - минимальное значение
+ * @param {number} max - максимальное значение
+ * @see https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ * @returns {number}
+ */
+const getRandomInclusiveInt = (min, max) => {
+  if ((typeof min !== 'number' || typeof max !== 'number') || (min < 0 && max < 0)) {
+    return null;
+  }
 
-// Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-
-// Целые числа
-
-const getRandomIntInclusive = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
+
+  if (min < 0) {
+    min = 0;
+  }
+
+  if (max < 0) {
+    max = 0;
+  }
+
+  if (min === max) {
+    return min;
+  }
+
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-}
+};
 
-// Числа с плавающей точкой
+/**
+ * Получаем случайное целое число из диапазона
+ * @param {number} min - минимальное значение
+ * @param {number} max - максимальное значение
+ * @param {number} precision - количество знаков после запятой
+ * @see https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ * @returns {number}
+ */
+const getRandomInclusiveNumber = (min, max, precision) => {
+  if ((typeof min !== 'number' || typeof max !== 'number') || (min < 0 && max < 0)) {
+    return null;
+  }
 
-const getRandom = (min, max, n) => {
-  return +((Math.random() * (max - min + 1)) + min).toFixed(n); //Максимум и минимум включаются
-}
+  if (min < 0) {
+    min = 0;
+  }
+
+  if (max < 0) {
+    max = 0;
+  }
+
+  if (min === max) {
+    return min;
+  }
+
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+
+  return +((Math.random() * (max - min + 1)) + min).toFixed(precision); //Максимум и минимум включаются
+};
+
+getRandomInclusiveInt();
+getRandomInclusiveNumber();
