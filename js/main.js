963a940c79c2a8e4484d39b655aaa73ef2b400fb
Ilya Gruzdev <gruzdevilyaa@gmail.com>
2022-03-10 17:47:02 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -66,3 +66,44 @@ const getRandomInclusiveNumber = (min, max, precision) => {
 
 getRandomInclusiveInt();
 getRandomInclusiveNumber();
+
+
+const createPromo = () => {
+  const typeHotel = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+  const timeToIn = ['12:00', '13:00', '14:00'];
+  const timeToOut = ['12:00', '13:00', '14:00'];
+  const featuresInHotel = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+  const locationLat = getRandomInclusiveNumber(35.65000, 35.70000, 5);
+  const locationLng = getRandomInclusiveNumber(139.70000, 139.80000, 5);
+
+  return {
+    author: {
+      avatar: "img/avatars/user{{xx}}.png",
+    },
+
+    offer: {
+      title: 'Отель',
+      address: [locationLat, locationLng],
+      price: getRandomInclusiveInt(0, 10),
+      type: typeHotel[getRandomInclusiveInt(0, typeHotel.length - 1)],
+      rooms: getRandomInclusiveInt(0, 10),
+      guests: getRandomInclusiveInt(0, 10),
+      checkin: timeToIn[getRandomInclusiveInt(0, timeToIn.length - 1)],
+      checkout: timeToOut[getRandomInclusiveInt(0, timeToOut.length - 1)],
+      features: featuresInHotel[getRandomInclusiveInt(0, featuresInHotel.length - 1)],
+      description: 'Пока ещё не придумал описание',
+      photos: '',
+    },
+
+    location: {
+      lat: locationLat,
+      lng: locationLng,
+    },
+  }
+}
+
+console.log(createPromo());
+
+const createPromoList = Array.from({length: 10}, createPromo);
+
+console.log(createPromoList);
