37a317d9b26b54f7c1414089e1d7bc6ed3e79791
Keks <keks@htmlacademy.ru>
2022-03-28 22:30:22 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/data.js
@@ -0,0 +1,28 @@
+export const ADDRESS_MIN_LAT = 35.65;
+export const ADDRESS_MAX_LAT = 35.7;
+export const ADDRESS_MIN_LNG = 139.7;
+export const ADDRESS_MAX_LNG = 139.8;
+export const COORDINATE_PRECISION = 5;
+export const OFFER_MIN_PRICE = 3000;
+export const OFFER_MAX_PRICE = 5000;
+export const HOTEL_MIN_ROOMS = 1;
+export const HOTEL_MAX_ROOMS = 5;
+export const OFFER_MIN_GUESTS = 1;
+export const OFFER_MAX_GUESTS = 10;
+export const HOTEL_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+export const OFFER_TO_IN_TIMES = ['12:00', '13:00', '14:00'];
+export const OFFER_TO_OUT_TIMES = ['12:00', '13:00', '14:00'];
+export const HOTEL_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+export const HOTEL_PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+export const ADVERTS_COUNT = 5;
+export const HotelTypes = {
+  palace: 'Дворец',
+  flat: 'Квартира',
+  house: 'Дом',
+  bungalow: 'Бунгало',
+  hotel: 'Отель',
+};
---
+++ 6/js/main.js
@@ -0,0 +1,6 @@
+import {createAdvert} from './create-advert.js';
+import {renderAdvert} from './render.js';
+import {ADVERTS_COUNT} from './data.js';
+const adverts = Array.from({length: ADVERTS_COUNT}, createAdvert);
+
+renderAdvert(adverts[0]);
