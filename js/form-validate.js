c9e917fec9254c3ad5391cb171b94e9895541ad3
Ilya Gruzdev <gruzdevilyaa@gmail.com>
2022-04-04 00:03:24 +0300

    Правки

--- js/form-validate.js
+++ js/form-validate.js
@@ -20,6 +20,9 @@ form.addEventListener('submit', (evt) => {
 
 const capacitySelectElement = document.querySelector('#capacity');
 const roomsSelectElement = document.querySelector('#room_number');
+const type = document.querySelector('#type');
+const minPriceElement = document.querySelector('#price');
+
 const validateCapacity = (value) => {
   const roomsValue = roomsSelectElement.value;
   const roomsCapacity = capacityRestrictions[roomsValue];
@@ -30,9 +33,10 @@ roomsSelectElement.addEventListener('change', () => {
   pristine.validate();
 });
 
+const validateMinPrice = (value) => Number(value) >= Number(minPriceElement.min);
+
 pristine.addValidator(capacitySelectElement, validateCapacity, 'Количество гостей не соответствует количеству комнат');
-
-
+pristine.addValidator(minPriceElement, validateMinPrice, 'Ошибка');
 
 const typeHousing = {
   'bungalow': 0,
@@ -42,34 +46,10 @@ const typeHousing = {
   'palace': 10000,
 };
 
-const type = document.querySelector('#type');
-const minPrice = document.querySelector('#price');
-
 const setPlaceholder = () => {
-  if (type.value === 'bungalow') {
-    minPrice.removeAttribute('placeholder');
-    minPrice.setAttribute('placeholder', `${typeHousing.bungalow}`);
-  }
-
-  if (type.value === 'flat') {
-    minPrice.removeAttribute('placeholder');
-    minPrice.setAttribute('placeholder', `${typeHousing.flat}`);
-  }
-
-  if (type.value === 'hotel') {
-    minPrice.removeAttribute('placeholder');
-    minPrice.setAttribute('placeholder', `${typeHousing.hotel}`);
-  }
-
-  if (type.value === 'house') {
-    minPrice.removeAttribute('placeholder');
-    minPrice.setAttribute('placeholder', `${typeHousing.house}`);
-  }
-
-  if (type.value === 'palace') {
-    minPrice.removeAttribute('placeholder');
-    minPrice.setAttribute('placeholder', `${typeHousing.palace}`);
-  }
+  minPriceElement.setAttribute('placeholder', typeHousing[type.value]);
+  minPriceElement.setAttribute('min', typeHousing[type.value]);
+  minPriceElement.dispatchEvent(new Event('input'));
 };
 
 type.addEventListener('change', setPlaceholder);
