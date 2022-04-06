ac6687616758acad556d1350bc766b0a720f7120
Ilya Gruzdev <gruzdevilyaa@gmail.com>
2022-04-06 13:00:26 +0300

    Добавляем функцию неактивности страницы

--- js/form-validate.js
+++ js/form-validate.js
@@ -2,6 +2,28 @@ import {capacityRestrictions} from './data.js';
 import {typeHousing} from './data.js';
 
 const form = document.querySelector('.ad-form');
+const capacitySelectElement = document.querySelector('#capacity');
+const roomsSelectElement = document.querySelector('#room_number');
+const type = document.querySelector('#type');
+const minPriceElement = document.querySelector('#price');
+const timeIn = document.querySelector('#timein');
+const timeOut = document.querySelector('#timeout');
+const formFieldsets = document.querySelectorAll('.ad-form__element');
+const formHeaderFieldset = document.querySelector('.ad-form-header');
+const mapFiltersContainer = document.querySelector('.map__filters');
+// Неактивное состояние страницы
+
+function pageInactive () {
+  mapFiltersContainer.classList.add('ad-form--disabled');
+  form.classList.add('ad-form--disabled');
+  formHeaderFieldset.setAttribute('disabled', 'disabled');
+  for (let i = 0; i <= formFieldsets.length; i++) {
+    formFieldsets[i].setAttribute('disabled', 'disabled');
+  }
+}
+
+pageInactive();
+
 const pristineConfig = {
   classTo: 'ad-form__element',
   errorClass: 'ad-form__element--invalid',
@@ -18,13 +40,6 @@ form.addEventListener('submit', (evt) => {
     evt.preventDefault();
   }
 });
-
-const capacitySelectElement = document.querySelector('#capacity');
-const roomsSelectElement = document.querySelector('#room_number');
-const type = document.querySelector('#type');
-const minPriceElement = document.querySelector('#price');
-const timeIn = document.querySelector('#timein');
-const timeOut = document.querySelector('#timeout');
 
 const validateCapacity = (value) => {
   const roomsValue = roomsSelectElement.value;
@@ -56,3 +71,4 @@ timeIn.addEventListener('change', () => {
 timeOut.addEventListener('change', () => {
   timeIn.value = timeOut.value;
 });
+
